005ba379e9f98ca41c864591ec6fefd83fdb10ab
Flavio luis silva almeida <flavioverd@gmail.com>
2019-02-12 14:45:47 -0200

    finalizado funções

---
+++ EXERCICIOS-JS/funcao/factory1.js
@@ -0,0 +1,9 @@
+// Factory simples // é uma função para instanciar objetos de forma automatica.
+function criarPessoa() {
+    return {
+    nome: 'Ana',
+    sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
