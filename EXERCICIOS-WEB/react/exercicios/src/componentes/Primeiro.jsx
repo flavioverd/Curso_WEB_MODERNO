b0af2bd1160b613bfa5b2579af56320371e4568b
Flavio luis silva almeida <flavioverd@gmail.com>
2019-06-14 00:53:40 -0300

    finalizado seção HTML

---
+++ EXERCICIOS-WEB/react/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export default () => <h1>Primeiro 2</h1>
+
---
+++ EXERCICIOS-WEB/react/exercicios/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import BomDia from './componentes/BomDia'
+
+ReactDOM.render(<BomDia nome="Flávio"/>, document.getElementById('root'))
+
