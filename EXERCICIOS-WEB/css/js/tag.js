1b1610c7493b087e6e2a8ac1f8d230ba41465fea
Flavio Luis <flavioverd@gmail.com>
2019-07-31 11:10:51 -0300

    salvando repositório para continuar em outro PC

---
+++ EXERCICIOS-WEB/css/js/tag.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.tag').forEach(elemento => {
+    const tagName = elemento.tagName.toLowerCase()
+
+    elemento.style.bodercolor = '616161'
+
+    if (!elemento.classList.contains('nolabel')) {
+        const label = document.createElement('label')
+        label.style.backgroundColor = '#616161'
+        label.innerHTML = tagName
+    elemento.insertBefore(label, elemento.childNodes[0])
+
+    }
+})
